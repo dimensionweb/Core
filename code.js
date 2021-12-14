9d034bb4ab315a1d9778955bbb06f4a25175f679
mng cluster <dimensionwebstudio@gmail.com>
2021-12-14 21:15:48 +0000

    master

--- code.js
+++ code.js
@@ -1,5 +1,10 @@
 function home() { window.location.replace("index.html"); }
-function projects() { document.getElementById("projects-tunel").style.display = "flex"; }
-function price() { document.getElementById("price-tunel").style.display = "flex"; }
-function aboutus() { document.getElementById("aboutus-tunel").style.display = "flex"; }
-function login() { document.getElementById("login-tunel").style.display = "flex"; }
+
+function projects() { if (document.getElementById("projects-tunel").style.display = "flex") {document.getElementById("projects-tunel").style.display = "flex";} else {document.getElementById("projects-tunel").style.display = "none";} }
+
+function price() {document.getElementById("price-tunel").style.display = "flex";}
+
+function aboutus() {document.getElementById("aboutus-tunel").style.display = "flex";}
+
+function login() {document.getElementById("login-tunel").style.display = "flex";}
+
