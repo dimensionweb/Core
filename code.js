2043363afac1f81831572e9a7cf82e0406a8e5a9
mng cluster <dimensionwebstudio@gmail.com>
2021-12-14 22:37:16 +0000

    master

--- code.js
+++ code.js
@@ -1,16 +1,17 @@
 function home() { window.location.replace("index.html"); }
 
-function projects() { 
-    if 
-    (document.getElementById("projects-tunel").style.display = "flex")
-    {document.getElementById("projects-tunel").style.display = "none";} 
-    else 
-    {document.getElementById("projects-tunel").style.display = "flex";} 
-} 
+if (document.getElementById("projects-tunel").style.display = "none") {
+    document.getElementById("projects-tunel").style.display = "flex";
+  } else {window.location.replace("index.html");}
 
-function price() {document.getElementById("price-tunel").style.display = "flex";}
+  if (document.getElementById("price-tunel").style.display = "none") {
+    document.getElementById("price-tunel").style.display = "flex";
+  } else {window.location.replace("index.html");}
 
-function aboutus() {document.getElementById("aboutus-tunel").style.display = "flex";}
+  if (document.getElementById("aboutus-tunel").style.display = "none") {
+    document.getElementById("aboutus-tunel").style.display = "flex";
+  } else {window.location.replace("index.html");}
 
-function login() {document.getElementById("login-tunel").style.display = "flex";}
-
+  if (document.getElementById("login-tunel").style.display = "none") {
+    document.getElementById("login-tunel").style.display = "flex";
+  } else {window.location.replace("index.html");}
