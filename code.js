e15216a828514e4529713c8aa004d7a42e47036f
mng cluster <dimensionwebstudio@gmail.com>
2021-12-14 21:09:39 +0000

    master

---
+++ code.js
@@ -0,0 +1,5 @@
+function home() { window.location.replace("index.html"); }
+function projects() { document.getElementById("projects-tunel").style.display = "flex"; }
+function price() { document.getElementById("price-tunel").style.display = "flex"; }
+function aboutus() { document.getElementById("aboutus-tunel").style.display = "flex"; }
+function login() { document.getElementById("login-tunel").style.display = "flex"; }
