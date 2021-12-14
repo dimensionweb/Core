56e6ba5c7a974d2a10ffca04166f3efb78392d3f
mng cluster <dimensionwebstudio@gmail.com>
2021-12-14 22:39:10 +0000

    master

--- code.js
+++ code.js
@@ -1,8 +1,6 @@
 function home() { window.location.replace("index.html"); }
 
-if (document.getElementById("projects-tunel").style.display = "none") {
-    document.getElementById("projects-tunel").style.display = "flex";
-  } else {window.location.replace("index.html");}
+if (document.getElementById("projects-tunel").style.display = "none") document.getElementById("projects-tunel").style.display = "flex"; else {window.location.replace("index.html");}
 
   if (document.getElementById("price-tunel").style.display = "none") {
     document.getElementById("price-tunel").style.display = "flex";
