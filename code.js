8739214bd3eb4b0bf68af418d70801fa978d9b14
mng cluster <dimensionwebstudio@gmail.com>
2021-12-17 14:43:24 +0000

    Login public

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ function home() { window.location.replace("index.html"); }
 function projects() { window.location.replace("projects.html"); }
 function price() { window.location.replace("price.html"); }
 function aboutus() { window.location.replace("aboutus.html"); }
-function login() { window.location.replace("login.html"); }
+function login() { window.location.replace("https://gitpod.io/#https://github.com/dimensionweb/Core"); }
 //NAVBAR FUNCTIONS CALLBACKS END /////////////////////////////
 
 
