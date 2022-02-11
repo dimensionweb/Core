be295bf9ebdf40c2d39d77de361a80d2c2733764
mng cluster <dimensionwebstudio@gmail.com>
2022-02-11 19:24:08 +0000

    update

--- Code/code.js
+++ Code/code.js
@@ -4,11 +4,11 @@
 
 //NAVBAR FUNCTIONS CALLBACKS //////////////////////////////////
 function home() { window.location.replace("https://dimensionweb.github.io/Core/"); }
-function projects() { window.location.replace("projects.html"); }
-function price() { window.location.replace("price.html"); }
-function aboutus() { window.location.replace("aboutus.html"); }
-function login() { window.location.replace("login.html"); }
-function CloudDeploy() { window.location.replace("CloudDeploy.html"); }
+function projects() { window.location.replace("/Pages/projects.html"); }
+function price() { window.location.replace("/Pages/price.html"); }
+function aboutus() { window.location.replace("/Pages/aboutus.html"); }
+function login() { window.location.replace("/Pages/login.html"); }
+function CloudDeploy() { window.location.replace("/Pages/CloudDeploy.html"); }
 //NAVBAR FUNCTIONS CALLBACKS END /////////////////////////////
 
 
