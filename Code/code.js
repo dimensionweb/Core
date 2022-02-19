96c41ef01ed9ed10579f7296d8f8a798e31b9f9e
mng cluster <dimensionwebstudio@gmail.com>
2022-02-19 21:05:46 +0000

    update

--- Code/code.js
+++ Code/code.js
@@ -7,7 +7,7 @@ function home() { window.location.replace("https://dimension.codes/"); }
 function projects() { window.location.replace("/Pages/projects.html"); }
 function price() { window.location.replace("/Pages/price.html"); }
 function aboutus() { window.location.replace("/Pages/aboutus.html"); }
-function login() { window.location.replace("/Pages/login.html"); }
+function login() { window.location.replace("https://gitpod.io/workspaces"); }
 function CloudDeploy() { window.location.replace("/Pages/CloudDeploy.html"); }
 //NAVBAR FUNCTIONS CALLBACKS END /////////////////////////////
 
