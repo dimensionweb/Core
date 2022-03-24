b197bbce301f9b401d8a35e4e082378664c1e57a
mng cluster <dimensionwebstudio@gmail.com>
2022-03-24 23:34:23 +0000

    update

--- Code/code.js
+++ Code/code.js
@@ -3,14 +3,22 @@
 ////////////////////////////////////////////////////////////////
 
 //NAVBAR FUNCTIONS CALLBACKS //////////////////////////////////
-function home() { window.location.replace("https://dimension.codes/"); }
-function projects() { window.location.replace("/Pages/projects.html"); }
-function price() { window.location.replace("/Pages/price.html"); }
-function aboutus() { window.location.replace("/Pages/aboutus.html"); }
-function login() { window.location.replace("https://gitpod.io/workspaces"); }
-function CloudDeploy() { window.location.replace("/Pages/CloudDeploy.html"); }
+function home() {
+  window.location.replace("https://dimension.codes/");
+}
+function projects() {
+  window.location.replace("/Pages/projects.html");
+}
+function price() {
+  window.location.replace("/Pages/price.html");
+}
+function aboutus() {
+  window.location.replace("/Pages/aboutus.html");
+}
+function login() {
+  window.location.replace("https://gitpod.io/workspaces");
+}
+function CloudDeploy() {
+  window.location.replace("/Pages/CloudDeploy.html");
+}
 //NAVBAR FUNCTIONS CALLBACKS END /////////////////////////////
-
-//SCROLLZOOM FUNCTIONS CALLBACKS /////////////////////////////
-
-//SCROLLZOOM FUNCTIONS CALLBACKS /////////////////////////////
