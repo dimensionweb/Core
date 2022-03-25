bd320c5b4a37e068e5ee93a7904f9658420d30b0
mng cluster <dimensionwebstudio@gmail.com>
2022-03-25 13:25:21 +0000

    update

--- Code/code.js
+++ Code/code.js
@@ -22,3 +22,30 @@ function CloudDeploy() {
   window.location.replace("/Pages/CloudDeploy.html");
 }
 //NAVBAR FUNCTIONS CALLBACKS END /////////////////////////////
+
+
+//PORTAL FUNCTIONS CALLBACKS END /////////////////////////////
+window.onload = addListeners;
+
+function addListeners(){
+    document.getElementById('portal').addEventListener('mousedown', mouseDown, false);
+    window.addEventListener('mouseup', mouseUp, false);
+
+}
+
+function mouseUp()
+{
+    window.removeEventListener('mousemove', divMove, true);
+}
+
+function mouseDown(e){
+  window.addEventListener('mousemove', divMove, true);
+}
+
+function divMove(e){
+    var div = document.getElementById('portal');
+  div.style.position = 'absolute';
+  div.style.top = e.clientY + 'px';
+  div.style.left = e.clientX + 'px';
+}​
+//PORTAL FUNCTIONS CALLBACKS END /////////////////////////////
