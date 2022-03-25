badc5e4a9b4099c9dcd90d368cfb9c716c4f6ca8
mng cluster <dimensionwebstudio@gmail.com>
2022-03-25 13:53:03 +0000

    update

--- Code/code.js
+++ Code/code.js
@@ -27,25 +27,23 @@ function CloudDeploy() {
 //PORTAL FUNCTIONS CALLBACKS END /////////////////////////////
 window.onload = addListeners;
 
-function addListeners(){
-    document.getElementById('portal').addEventListener('mousedown', mouseDown, false);
-    window.addEventListener('mouseup', mouseUp, false);
-
+function addListeners() {
+  document.getElementById('portal').addEventListener('mousedown', mouseDown, false);
+  window.addEventListener('mouseup', mouseUp, false);
 }
 
-function mouseUp()
-{
-    window.removeEventListener('mousemove', divMove, true);
+function mouseUp() {
+  window.removeEventListener('mousemove', divMove, true);
 }
 
-function mouseDown(e){
+function mouseDown(e) {
   window.addEventListener('mousemove', divMove, true);
 }
 
-function divMove(e){
-    var div = document.getElementById('portal');
+function divMove(e) {
+  var div = document.getElementById('portal');
   div.style.position = 'absolute';
   div.style.top = e.clientY + 'px';
   div.style.left = e.clientX + 'px';
-}​
+}
 //PORTAL FUNCTIONS CALLBACKS END /////////////////////////////
