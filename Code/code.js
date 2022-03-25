d3aaf21af560b5980307051f5c0f2a26e7552cfe
mng cluster <dimensionwebstudio@gmail.com>
2022-03-25 22:22:49 +0000

    update

--- Code/code.js
+++ Code/code.js
@@ -31,3 +31,34 @@ function CloudDeploy() {
 }
 
 //NAVBAR FUNCTIONS CALLBACKS END /////////////////////////////
+
+//DEPLOY SERVER //////////////////////////////////////////////
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  function UploadImage() {
+    var preview = document.getElementById('show-code');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader()
+
+    var textFile = /text.*/;
+
+    if (file.type.match(textFile)) {
+      reader.onload = function (event) {
+        preview.innerHTML = event.target.result;
+        document.getElementById("brain").style.opacity = "1";
+        document.getElementById("plus").style.visibility = "hidden";
+      }
+    } else {
+      preview.innerHTML = "<span class='error'>DimensionCloud can only deploy .txt [ docker-compose ]</span>";
+      document.getElementById("brain").style.opacity = "0.1";
+      wait(1);
+      window.location.replace("/Pages/CloudDeploy.html");
+    }
+    reader.readAsText(file);
+  }
+} else {
+  alert("Your browser is too old to support HTML5 File API");
+  document.getElementById("brain").style.opacity = "0.1";
+  wait(1);
+  window.location.replace("/Pages/CloudDeploy.html");
+}
+ //DEPLOY SERVER //////////////////////////////////////////////
